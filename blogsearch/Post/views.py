891a108a7086f590b2096cb3437de66cf6572123
Mike Mather <michael_mather@hotmail.com>
2021-05-27 22:49:53 -0230

    Fix search result ranking

--- blogsearch/Post/views.py
+++ blogsearch/Post/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Post
+from itertools import chain
 
 def search(request):
     query = request.GET.get('q', '')
     print('Search query: ', query)
     if query:
         query = query.lower()
-        title_result = Post.objects.filter(title__icontains=query).values('id', 'title', 'description', 'url', 'feed__name', 'created_at')
-        vector_result = Post.objects.filter(search_vector=query).values('id', 'title', 'description', 'url', 'feed__name', 'created_at')
+        title_result = Post.objects.filter(title__icontains=query) \
+            .values('id', 'title', 'description', 'url', 'feed__name', 'created_at')
+        vector_result = Post.objects.filter(search_vector=query) \
+            .values('id', 'title', 'description', 'url', 'feed__name', 'created_at') \
+            .order_by('-created_at')
         print(f'title result: {len(title_result)}. vector result: {len(vector_result)}')
-        result = title_result.order_by('-created_at') | vector_result.order_by('-created_at')
-        result = result.distinct()
-        result = list(result)
+        result = list(title_result) + list(vector_result)
+        result = [dict(x) for x in {tuple(d.items()) for d in result}]
         return JsonResponse(result, safe=False)
     else:
         response = {'message': 'Expected query parameters'}
--- frontend/src/api/Api.ts
+++ frontend/src/api/Api.ts
@@ -1,8 +1,8 @@
 import { ResultType } from "../types";
 import formatDistance from 'date-fns/formatDistance';
 
-export const endpoint = 'https://blogsearch-prod.herokuapp.com';
-//export const endpoint = 'http://localhost:8000';
+//export const endpoint = 'https://blogsearch-prod.herokuapp.com';
+export const endpoint = 'http://localhost:8000';
 
 const processResults = (posts: any[]): ResultType[] => {
     return posts.map((post: ResultType) => {
